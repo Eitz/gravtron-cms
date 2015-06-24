42d5037f93b176e117b012b56407a0b361437e1a
Eitz <richard.eitz@gravidade.org>
2015-06-24 10:51:53 -0300

    Post page done, added Disqus to posts

--- cms.pl
+++ cms.pl
@@ -18,74 +18,6 @@ use DateTime;
 my $POST_HOME = "$FindBin::Bin/posts";
 my $PREFIX = '/devaneios';
 
-app->config(
-	hypnotoad => {listen => ['http://*:3000']}
-);
-
-helper dt => sub {
-	return "DateTime";
-};
-
-helper find_categories => sub {
-    my ($c) = @_;
-    
-    my $dir = "$POST_HOME";
-
-    opendir(my $dh, $dir) or return undef;
-    my @dirs = 
-    			grep { -d "$POST_HOME/$_" && !/\..?/ } readdir($dh);
-    closedir($dh);
-    return @dirs;
-};
-
-helper find_posts => sub {
-    my ($c, $categoria) = @_;
-    
-    my $dir = "$POST_HOME/$categoria";
-
-    opendir(my $dh, $dir) or return undef;
-    my @files = 
-    			map { s/\.post//; $_ }
-    			grep { /\.post$/ } readdir($dh);
-    closedir($dh);
-    return @files;
-};
-
-helper read_post => sub {
-	my ($c, $categoria, $post, $extra) = @_;
-
-	my $filename = "$POST_HOME/$categoria/$post.post";
-
-	my $file;
-	{ my $fh;
-	  local $/ = undef;
-	  open $fh, '<:encoding(utf8)', $filename;
-	  $file = <$fh>;
-	  close $fh;
-	}
-
-	my %info;
-
-	my ($info_text, $body) = split '↓↓↓', $file;
-	for my $line (split '\n', $info_text){
-		my ($key, $value) = split ':', $line;
-		$key =~ s/^\s+//;
-		$key =~ s/\s+$//;
-		$value =~ s/^\s+//;
-		$value =~ s/\s+$//;
-		if ($key eq "date"){
-			my @arr = split '-', $value;
-			$value = DateTime->new(year => $arr[0], month => $arr[1], day => $arr[2], locale => "pt_BR");
-		}
-		$info{$key} = $value;
-	}
-
-	$info{categoria} = $categoria;
-	$info{post_slug} = $post;
-	$info{url} = "$PREFIX/$categoria/$post";
-	return \%info, $body;
-};
-
 get '/' => sub {
 	my $c = shift;
 	
@@ -141,23 +73,105 @@ get "$PREFIX" => sub {
 
 get "$PREFIX/:categoria/:post" => sub {
 	my $c = shift;
-	
+
 	my $categoria = $c->param('categoria');
 	my $post = $c->param('post');
 
-	my $cat_path = "$POST_HOME/$categoria";
-	my $post_path = "$cat_path/$post.post";
+	my ($info, $body) = $c->read_post($categoria, $post);
 
-	unless (-d $cat_path || -f $post_path) {
+	unless ($info && $body) {
 		return $c->reply->not_found
 	}
 
-    my ($info, $body) = $c->read_post($categoria, $post);
-
-
-    $c->stash(info => $info);
-    $c->stash(body => $body);
+	$c->stash(info => $info, body => $body);	
 	return $c->render('post');
 };
+
+helper dt => sub {
+	return "DateTime";
+};
+
+helper find_categories => sub {
+    my ($c) = @_;
+    
+    my $dir = "$POST_HOME";
+
+    opendir(my $dh, $dir) or return undef;
+    my @dirs = grep { -d "$POST_HOME/$_" && !/\..?/ } readdir($dh);
+    closedir($dh);
+    return @dirs;
+};
+
+helper find_posts => sub {
+    my ($c, $categoria) = @_;
+    
+    my $dir = "$POST_HOME/$categoria";
+
+    opendir(my $dh, $dir) or return undef;
+    my @files = 
+    			map { s/\.post//; $_ }
+    			grep { /\.post$/ } readdir($dh);
+    closedir($dh);
+    return @files;
+};
+
+helper read_post => sub {
+	my ($c, $categoria, $post, $extra) = @_;
+
+	my $filename = "$POST_HOME/$categoria/$post.post";
+	my $file = $c->open_file($filename);
+
+	unless ($file) {
+		return undef, undef;
+	}
+
+	my ($info_text, $body) = split '↓↓↓', $file;
+
+	my $info = {
+		categoria => $categoria,
+		post_slug => $post,
+		url => "$PREFIX/$categoria/$post"
+	};
+	
+	for my $line (split '\n', $info_text){
+
+		my ($key, $value) = split ':', $line;
+		
+		$key =~ s/^\s+//; $key =~ s/\s+$//;
+		$value =~ s/^\s+//; $value =~ s/\s+$//;
+
+		if ($key eq 'date'){
+			my @arr = split '-', $value;
+			$value = eval{ 
+						$c->dt->new(
+							year 	=> $arr[0],
+							month 	=> $arr[1],
+							day 	=> $arr[2],
+							locale 	=> "pt_BR"
+						)
+		 			};
+		}
+		$info->{$key} = $value;
+	}
+
+	
+	return $info, $body;
+};
+
+helper open_file => sub {
+	my ($c, $filename) = @_;
+	my $file;
+	{ my $fh;
+	  local $/ = undef;
+	  open $fh, '<:encoding(utf8)', $filename;
+	  $file = <$fh>;
+	  close $fh;
+	}
+	return $file;
+};
+
+app->config(
+	hypnotoad => {listen => ['http://*:3000']}
+);
 
 app->start;
